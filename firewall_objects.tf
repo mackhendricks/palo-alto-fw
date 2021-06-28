ea6621fdafac3637f8b9d16d2b6649e1e30edfe1
Mack Hendricks <mack@dopensource.com>
2021-06-28 11:46:00 -0400

    Create firewall_objects.tf

---
+++ firewall_objects.tf
@@ -0,0 +1,19 @@
+# Static group
+resource "panos_address_group" "example1" {
+    name = "static ntp grp"
+    description = "My NTP servers"
+    static_addresses = [
+        panos_address_object.ao1.name,
+        panos_address_object.ao2.name,
+    ]
+}
+
+resource "panos_address_object" "ao1" {
+    name = "ntp1"
+    value = "10.0.2.1"
+}
+
+resource "panos_address_object" "ao2" {
+    name = "ntp2"
+    value = "10.0.2.2"
+}
